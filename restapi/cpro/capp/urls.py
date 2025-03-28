bbaaac94a092e3859a13bda44e9d94c174e0f031
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-03-28 16:31:31 +0530

    restapi created

---
+++ restapi/cpro/capp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.fun2,name='fun2'),
+    path('fun3',views.fun3,name='fun3'),
+    path('fun4/<int:d>',views.fun4,name='fun4'),
+]
